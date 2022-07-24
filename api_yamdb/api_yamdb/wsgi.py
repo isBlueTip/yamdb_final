3f8cd5eb5c57f7391e54f4f3b7a080e493f6cc84
Simon Egorov <rhinoRofl@gmail.com>
2022-07-24 18:39:29 +0700

    Comments deletion

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -4,14 +4,3 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 application = get_wsgi_application()
-
-# import os
-# import sys
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# sys.path.append(BASE_DIR)
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
-# application = get_wsgi_application()
