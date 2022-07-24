ff2db649e43d36fb34744fb85987556962c1b97b
Simon Egorov <rhinoRofl@gmail.com>
2022-07-24 18:14:02 +0700

    Experiments with dockerfile

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -1,15 +1,17 @@
-# import os
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-# 'api_yamdb.api_yamdb.settings')
-
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 application = get_wsgi_application()
+
+# import os
+# import sys
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
+# application = get_wsgi_application()
