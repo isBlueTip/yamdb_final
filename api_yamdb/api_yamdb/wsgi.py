ae9d95896f9ab6a5b8e9cc7f7c88ac6732aada11
Simon Egorov <rhinoRofl@gmail.com>
2022-07-24 17:57:32 +0700

    Experiment with wsgi.py

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -1,7 +1,15 @@
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.api_yamdb.settings')
+#
+# application = get_wsgi_application()
+
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.api_yamdb.settings')
-
-application = get_wsgi_application()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
