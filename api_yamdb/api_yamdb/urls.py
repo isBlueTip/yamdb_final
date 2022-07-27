c32e2ca7bac22022b3fa3afdffecb572303179c5
Simon Egorov <rhinoRofl@gmail.com>
2022-07-27 10:28:55 +0700

    Add redoc to urls.py

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-# from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -9,20 +8,14 @@ from rest_framework import permissions
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # path(
-    #     'redoc/',
-    #     TemplateView.as_view(template_name='redoc.html'),
-    #     name='redoc'
-    # ),
 ]
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Cats API",
+        title="YamDB API",
         default_version='v1',
-        description="Документация для приложения cats проекта Kittygram",
-        # terms_of_service="URL страницы с пользовательским соглашением",
-        contact=openapi.Contact(email="admin@kittygram.ru"),
+        description="Документация для API проекта YamDB",
+        contact=openapi.Contact(email="rhinorofl@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -30,10 +23,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns += [
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc'),
 ]
