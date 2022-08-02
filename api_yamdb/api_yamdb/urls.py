c026369b634ec2f06e9f81439f391c581820bc45
Simon Egorov <rhinoRofl@gmail.com>
2022-08-02 13:23:40 +0700

    admin optional slash test

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -6,7 +6,7 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 urlpatterns = [
-    path('admin/?', admin.site.urls),
+    path(r'^admin/?$', admin.site.urls),
     path('api/', include('api.urls')),
 ]
 
