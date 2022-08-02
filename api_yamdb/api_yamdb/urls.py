b64f1028f6bb1ebbbc4c277dde5f6489e560eb41
Simon Egorov <rhinoRofl@gmail.com>
2022-08-02 13:12:55 +0700

    redoc optional slash test

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -24,5 +24,6 @@ schema_view = get_schema_view(
 
 urlpatterns += [
     url(r'^redoc/?$', schema_view.with_ui('redoc', cache_timeout=0),
+    # url(r'^\/redoc((\/\w+)+|\/?)$', schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc'),
 ]
