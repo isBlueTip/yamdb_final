c0b22d78fd4366b519f68c1a6514df7c0266da29
Simon Egorov <rhinoRofl@gmail.com>
2022-08-03 11:08:54 +0700

    Isort api_yamdb/api

--- api_yamdb/api/reviews_views.py
+++ api_yamdb/api/reviews_views.py
@@ -1,10 +1,11 @@
-from api.permissions import IsAdmin, IsAuthorOrReadOnly, IsModer
-from api.reviews_serializers import CommentSerializer, ReviewSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from reviews.models import Review
 from titles.models import Title
+
+from api.permissions import IsAdmin, IsAuthorOrReadOnly, IsModer
+from api.reviews_serializers import CommentSerializer, ReviewSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
